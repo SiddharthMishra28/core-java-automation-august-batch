2fd118dd1d3b72381c7f812fdf095f0c9af228a7
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2025-11-13 10:43:05 +0530

    Selenium 101 - Xpath Session 1

---
+++ src/test/java/com/selenium/browserinteractions/xpath/Assignment.java
@@ -0,0 +1,8 @@
+package com.selenium.browserinteractions.xpath;
+
+public class Assignment {
+    // OPEN CHROME DEV TOOLS CONSOLE
+    // LOCATE ALL ELEMENTS IN BRANCH MANAGER HOMEPAGE
+    // 1. USING XPATH
+    // 2. USING CSSPATH
+}
---
+++ src/test/java/com/selenium/browserinteractions/xpath/RelativeXpathDemo.java
@@ -0,0 +1,18 @@
+package com.selenium.browserinteractions.xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RelativeXpathDemo {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.guru99.com/V4/");
+        driver.findElement(By.name("uid")).sendKeys("mngr643764");
+        driver.findElement(By.name("password")).sendKeys("ujuturE");
+        driver.findElement(By.name("btnLogin")).click();
+        driver.findElement(By.xpath("//a[@href='addcustomerpage.php']")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
